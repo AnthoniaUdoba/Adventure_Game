8d94c18dc4fc39b7a805a5c42fdf6a54ca689e37
AnthoniaUdoba <99968773+AnthoniaUdoba@users.noreply.github.com>
2022-04-12 14:08:16 +0100

    Update adventure_game.py

--- adventure_game.py
+++ adventure_game.py
@@ -1,1 +1,9 @@
+import time
 
+def print_pause(message):
+    print(message)
+    time.sleep(2)
+
+def intro():    
+print_pause("You find yourself standing in an open field, filled with grass and yellow wildflowers.")  
+print_pause("Rumor has it that a wicked fairie is somewhere around here, and has been terrifying the nearby village.)
